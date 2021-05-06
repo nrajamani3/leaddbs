428982da8cd127b6685eaffab91f16e299833f3f
nrajamani3 <nanditharajamani111@gmail.com>
2021-05-06 10:38:29 +0200

    smoothing has been updated

--- MixedFibers/ea_nii2fv_v2.m
+++ MixedFibers/ea_nii2fv_v2.m
@@ -1,4 +1,4 @@
-function fv=ea_nii2fv_v2(nii_filename,smooth,smooth_value)
+function [fv,fv2]=ea_nii2fv_v2(nii_filename,smooth,smooth_value)
 
 nii = ea_load_nii(nii_filename);
 nii.img(isnan(nii.img))=0;
@@ -11,13 +11,14 @@ for dim=1:3
 end
 [X,Y,Z]=meshgrid(gv{1},gv{2},gv{3});
 
-if smooth
-    nii.img = smooth3(nii.img,'gaussian',smooth_value);
-    %options.prefs.hullsmooth);
-end
-%toc
-
 fv=isosurface(X,Y,Z,permute(nii.img,[2,1,3]),max(nii.img(:))/2);
 fvc=isocaps(X,Y,Z,permute(nii.img,[2,1,3]),max(nii.img(:))/2);
 fv.faces=[fv.faces;fvc.faces+size(fv.vertices,1)];
 fv.vertices=[fv.vertices;fvc.vertices];
+if smooth
+    tic
+    fv2 = ea_smoothpatch(fv,1,1,smooth_value,0);
+    toc
+else
+    fv2 = [];
+end
